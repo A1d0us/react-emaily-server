e81a264e49e6c79d82ff1a9fce460c169d53104c
a1d0us <diakonenko.vanya@gmail.com>
2024-12-26 16:43:48 +0100

    added google auth

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,8 @@ passport.use(
   new GoogleStrategy({
       clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
       clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({googleId: profile.id})
